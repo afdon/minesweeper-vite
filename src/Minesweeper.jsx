d1a35e0107873240c4575c98d8650d4d74986cfb
afdon <53676753+afdon@users.noreply.github.com>
2023-08-15 15:13:15 -0400

    deploy

--- src/Minesweeper.jsx
+++ src/Minesweeper.jsx
@@ -1,5 +1,4 @@
-
-"use client"
+"use client";
 
 import { useState } from "react";
 import {
@@ -28,8 +27,11 @@ let display = getDisplayValues(board);
 //////////
 
 export default function Game() {
-
-  let [settings, useSettings] = useState({ numMines: 25, numRows: 16, numCols: 30 }) // changed
+  let [settings, setSettings] = useState({
+    numMines: 25,
+    numRows: 16,
+    numCols: 30,
+  }); // changed
 
   const [revealedCells, setRevealedCells] = useState(
     new Array(settings.numRows * settings.numCols).fill(false)
@@ -53,6 +55,29 @@ export default function Game() {
     checkWin();
   };
 
+  const handleIncrement = (property) => {
+    let increment = 1;
+    if (property === "numMines") {
+      increment = 5;
+    }
+
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [property]: prevSettings[property] + increment,
+    }));
+  };
+
+  const handleDecrement = (property) => {
+    let increment = 1;
+    if (property === "numMines") {
+      increment = 5;
+    }
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [property]: prevSettings[property] - increment,
+    }));
+  };
+
   const handleCellClick = (index) => {
     // console.log(revealedCells[index], index);
 
@@ -73,14 +98,20 @@ export default function Game() {
     setRevealedCells(newRevCells);
   };
 
-  const total = settings.numRows * settings.numCols;
+  // const total = settings.numRows * settings.numCols;
+
+  const getTotal = (settings) => {
+    return settings.numRows * settings.numCols;
+  }
+
   let numRevealed = revealedCells.filter((cell) => cell === true).length;
   let numFlagged = flagged.filter((cell) => cell === true).length;
 
   let checkWin = () => {
+    console.log("numRevealed", numRevealed);
+    console.log("numFlagged", numFlagged);
 
-    console.log("numRevealed", numRevealed)
-    console.log("numFlagged", numFlagged)
+    let total = getTotal(settings)
 
     if (numRevealed + numFlagged >= total) {
       setGameState("won");
@@ -93,6 +124,62 @@ export default function Game() {
       {gameState === "won" && <p>You won!</p>}
 
       <div>Mines Left: {settings.numMines - numFlagged}</div>
+
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '4', 'justifyContent': 'center', margin: '5px' }} >
+        <div>
+          <button
+            property="numMines"
+            onClick={() => handleDecrement("numMines")}
+          >
+            -
+          </button>
+          <span style={{ display: "inline", margin: "5px", color: "white" }}>
+            {settings.numMines}
+          </span>
+          <button
+            property="numMines"
+            onClick={() => handleIncrement("numMines")}
+          >
+            +
+          </button>
+        </div>
+
+        <div>
+          <button
+            property="numRows"
+            onClick={() => handleDecrement("numRows")}
+          >
+            -
+          </button>
+          <span style={{ display: "inline", margin: "5px", color: "white" }}>
+            {settings.numRows}
+          </span>
+          <button
+            property="numRows"
+            onClick={() => handleIncrement("numRows")}
+          >
+            +
+          </button>
+        </div>
+
+        <div>
+          <button
+            property="numCols"
+            onClick={() => handleDecrement("numCols")}
+          >
+            -
+          </button>
+          <span style={{ display: "inline", margin: "5px", color: "white" }}>
+            {settings.numCols}
+          </span>
+          <button
+            property="numCols"
+            onClick={() => handleIncrement("numCols")}
+          >
+            +
+          </button>
+        </div>
+      </div>
 
       <div
         className="board"
@@ -147,7 +234,7 @@ const GameCell = (props) => {
       onContextMenu={props.onContextMenu}
     >
       {props.isRevealed && props.cellValue}
-      {(!props.isRevealed && props.isFlagged) && "🚩"}
+      {!props.isRevealed && props.isFlagged && "🚩"}
       {/* {props.isRevealed && `${getDisplayValue(props.cellValue)}`} */}
     </div>
   );
