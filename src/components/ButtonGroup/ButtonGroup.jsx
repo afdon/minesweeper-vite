c4ee27c6b7337ff4d83e307a0df1be37b111152e
afdon <53676753+afdon@users.noreply.github.com>
2023-08-17 10:25:33 -0400

    tweaks

--- src/components/ButtonGroup/ButtonGroup.jsx
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -3,21 +3,21 @@ import './ButtonGroup.css';
 const ButtonGroup = ({ onClick, rows, cols, mines }) => {
   return (
     <>
-      <div className="buttonGroup">
+      <div className='buttonGroup'>
         <div className="mines">
           <button onClick={() => onClick('decrement', 'mines')}>-</button>
           <span>{mines} M</span>
           <button onClick={() => onClick('increment', 'mines')}>+</button>
         </div>
 
-        <div className="rowsCols">
-          <div>
+        <div className='rowsCols'>
+          <div className='rows'>
             <button onClick={() => onClick('decrement', 'rows')}>-</button>
             <span>{rows} R</span>
             <button onClick={() => onClick('increment', 'rows')}>+</button>
           </div>
 
-          <div>
+          <div className='cols'>
             <button onClick={() => onClick('decrement', 'cols')}>-</button>
             <span>{cols} C</span>
             <button onClick={() => onClick('increment', 'cols')}>+</button>
